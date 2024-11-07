409b1e5e7ac88abce751d243e01b9ea5120613fa
a-riceeater <elijb12@gmail.com>
2024-11-07 08:42:25 -0800

    add padding

--- app/src/main/java/xyz/chronosirius/accordion/directs/DirectConversationScreen.kt
+++ app/src/main/java/xyz/chronosirius/accordion/directs/DirectConversationScreen.kt
@@ -4,12 +4,15 @@ import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.navigation.NavController
 import xyz.chronosirius.accordion.R
 import xyz.chronosirius.accordion.viewmodels.RequestViewModel
@@ -22,7 +25,7 @@ fun ConversationScreen(channelId: Int, vm: RequestViewModel, navController: NavC
         // will load a conversation screen fragment with the messages once loaded
         // vm.get { url("https://discord.com/api/v9/users/@me/channels") }
     }
-    Row {
+    Row (modifier = Modifier.padding(5.dp)) {
         Text("Chronosirirus")
         Spacer(modifier = Modifier.weight(1f))
         Icon(
